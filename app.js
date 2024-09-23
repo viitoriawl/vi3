9e02b8df3c076c777982adb7572ccedc14f51439
viitoriawl <161336717+viitoriawl@users.noreply.github.com>
2024-09-23 12:12:54 -0300

    JFGJ

---
+++ app.js
@@ -0,0 +1,7 @@
+function criaCartao(categoria,pergunta,rsposta){
+    let container=document.getElementByld('container')
+    let cartao=document.createElement('article')
+    cartao.className='cartao'
+cartao.innerHTML=`
+<div class=
+}
